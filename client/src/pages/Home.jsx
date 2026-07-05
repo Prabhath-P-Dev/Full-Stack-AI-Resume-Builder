fa9a330a507ff8ab9bd0660d3d97c13d894d24b9
Prabhath-P-Dev <prabhath0492@gmail.com>
2026-07-05 12:18:00 +0530

    Updated client folder

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "../components/home/Banner";
 import Hero from "../components/home/Hero";
 import Features from "../components/home/Features";
 import Testimonial from "../components/home/Testimonial";
-import CallToAction from "../components/home/CalltoAction";
+import CallToAction from "../components/home/CallToAction";
 import Footer from "../components/home/Footer";
 
 const Home = ()=>{
